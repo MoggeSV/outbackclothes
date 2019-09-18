ad5bcf0832fe084235e432ca655fdd26f8fb0e48
Morgan Svensson <morgan.svensson10@gmail.com>
2019-09-18 12:56:09 +0200

    added folders

---
+++ src/components/Topnav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container-fluid">
+      <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Topnav"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
