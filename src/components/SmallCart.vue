77323718f2c77d7bc888c7b560135136dcfb54b6
Totte <christoffer.bjorn.cajander@gmail.com>
2019-09-26 20:03:54 +0200

    Small fixes

--- src/components/SmallCart.vue
+++ src/components/SmallCart.vue
@@ -1,9 +1,11 @@
 <template>
      <div id="small-cart">
       <div id="small-cart-header">
-         <h2>Varukorg</h2>
+         <h6><span class="text-muted"><i class="fas fa-mountain"></i></span>Varukorg</h6>
       </div>
       <div id="small-cart-content">
+
+          <!-- List of products here -->
          <ul class="list-group">
             <li class="list-group-item">
                <span class="row">
@@ -11,23 +13,37 @@
                   <div class="col-9">Northface</div>
                </span>
                <span class="row">
-                  <div class="col-8"><span class="product-text">Vinterjacka</span></div>
-                  <div class="col-2 price">978:-</div>
-                  <div class="col-2"><i class="fas fa-minus-circle remove-icon"></i></div>
+                  <div class="col-7"><span class="product-text">Vinterjacka</span></div>
+                  <div class="col-3 price">970:-</div>
+                  <div class="col-2"><i class="fas fa-minus remove-icon text-muted"></i></div>
                </span>
             </li>
             <li class="list-group-item">
-               <span class="row">
+               <div class="row">
                   <div class="col-2">1</div>
                   <div class="col-9">Northface</div>
-               </span>
-               <span class="row">
-                  <div class="col-8"><span class="product-text">Vinterjacka</span></div>
-                  <div class="col-2 price">978:-</div>
-                  <div class="col-2"><i class="fas fa-minus-circle remove-icon"></i></div>
-               </span>
+               </div>
+               <div class="row">
+                  <div class="col-7"><span class="product-text">Vinterjacka</span></div>
+                  <div class="col-3 price">2700:-</div>
+                  <div class="col-2"><i class="fas fa-minus remove-icon text-muted"></i></div>
+               </div>
             </li>
           </ul>
+          <!-- End of product list -->
+            <div class="row mt-5">
+                <div class="col mt-3">
+                    <span>Total: </span>
+                </div>
+                <div class="col mt-3 d-flex justify-content-end">
+                    <span style="font-weight: bold">3784:-</span>
+                </div>
+            </div>
+          <div class="row mt-2">
+              <div class="col d-flex justify-content-end">
+                <router-link to="/checkout" class="btn checkout-btn"><i class="far fa-credit-card"></i> Gå vidare</router-link>
+              </div>
+          </div>
       </div>
    </div>
 </template>
@@ -46,6 +62,7 @@ box-shadow: 1px 1px 5px 0px rgba(0,0,0,0.38);
     border-radius: 5px;
     width: 250px;
     min-height: 300px;
+    min-width: 300px;
     padding: 10px;
     background: white;
 }
@@ -58,16 +75,30 @@ box-shadow: 1px 1px 5px 0px rgba(0,0,0,0.38);
     
 }
 .product-text {
-    color: rgb(84, 84, 84);
+    color: rgb(65, 65, 65);
     text-transform: uppercase;
+    font-weight: 500;
 }
 
 .remove-icon {
-    margin-left: 3px;
+    margin-left: 5px;
 }
 
 .price {
     font-weight: 700;
     color:black;
 }
+
+.checkout-btn {
+    background: #72BAB2;
+    color: white;
+}
+
+/* Används inte ATM */ 
+.shadow-top-inset {
+    box-shadow: inset 0 7px 9px -9px rgba(0,0,0,0.3);
+    box-sizing: content-box;
+    overflow: hidden;
+}
+
 </style>
