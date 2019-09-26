1337c566bcff0b16c69853246608496ff688a8db
Morgan Svensson <morgan.svensson10@gmail.com>
2019-09-26 11:09:14 +0200

    routes update

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/children">Children</router-link>
+
     </div>
     <router-view />
   </div>
