222b845a84233375c0145b9e78838e8629aca75b
Morgan Svensson <morgan.svensson10@gmail.com>
2019-09-26 11:12:44 +0200

    routes updates

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import About from './views/About'
-import Children from './components/Children'
+import About from './views/About.vue'
+import Children from './components/Children.vue'
 
 Vue.use(Router);
 
