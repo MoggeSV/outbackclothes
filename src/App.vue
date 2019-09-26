11eaa449d5cfc3199bb60535fc49e3de2ce8dc7d
Totte <christoffer.bjorn.cajander@gmail.com>
2019-09-26 11:38:04 +0200

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/children">Children</router-link> |
-      <router-link to="/men">Men</router-link> |
-      <router-link to="/woman">Woman</router-link> |
-      <router-link to="/checkout">Checkout</router-link>
+      <NavBar />
 
     </div>
     <router-view />
   </div>
 </template>
 <script>
+import NavBar from '@/components/NavBar'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    NavBar
+  }
 }
 </script>
 <style lang="scss" scoped>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,53 @@
+<template>
+   <nav class="navbar navbar-expand-lg navbar-light bg-white">
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+       </button>
+       <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="col d-flex justify-content-center">
+                <ul class="navbar-nav">
+                      <li class="nav-item">
+                         <a class="nav-link border-end" href="#">MAN</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link border-end" href="#">KVINNA</a>
+                      </li>
+                      <li class="nav-item">
+                         <a class="nav-link custom-line-height " href="#">BARN</a>
+                      </li>
+                   </ul>
+          </div>
+          <div class="col d-flex justify-content-center">
+                <a class="navbar-brand mx-auto" href="#">
+                 <img src="@/assets/images/oclogo.png" class="logo" />
+                  <span>OutbackClothes</span>
+                </a>
+          </div>
+          <div class="col d-flex justify-content-center">
+                <ul class="navbar-nav">
+                   <li class="nav-item">
+                      <a class="nav-link" href="#"><i class="far fa-user-circle"></i></a>
+                   </li>
+                   <li class="nav-item">
+                         <a class="nav-link" href="#"><i class="fas fa-shopping-bag"></i></a>
+                      </li>
+                </ul>
+          </div>
+       </div>
+       </nav>
+ </template>
+ 
+ <script>
+ 
+ 
+ export default {
+ name: "NavBar",
+ }
+ 
+ </script>
+ <style>
+ .logo {
+   width: 32px;
+   height: 32px;
+ }
+ </style>
