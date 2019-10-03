3c9c47f15a9e6a51a8d4f633ad2a4872945d3fd6
Jesper Björlestrand <bjorlestrand@gmail.com>
2019-10-03 11:28:52 +0200

    Klar

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,168 +1,122 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="slide">
-        <div id="ProjeCarousel" class="carousel slide" data-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <div class="col-md-4 float-left">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="http://via.placeholder.com/350x350"
-                    alt="Card image cap"
-                  />
-                  <div class="card-body" id="cardBody">
-                    <h4>Man</h4>
-                    <a href="#" class="card-link">Exempel plagg</a>
-                    <p>Exempel pris</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 float-left">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="http://via.placeholder.com/350x350"
-                    alt="Card image cap"
-                  />
-                  <div class="card-body" id="cardBody">
-                    <h4>Kvinna</h4>
-                    <a href="#" class="card-link">Exempel plagg</a>
-                    <p>Exempel pris</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 float-left">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="http://via.placeholder.com/350x350"
-                    alt="Card image cap"
-                  />
-                  <div class="card-body" id="cardBody">
-                    <h4>Barn</h4>
-                    <a href="#" class="card-link">Exempel plagg</a>
-                    <p>Exempel pris</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="carousel-item">
-              <div class="col-md-4 float-left">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="http://via.placeholder.com/350x350"
-                    alt="Card image cap"
-                  />
-                  <div class="card-body" id="cardBody">
-                    <h4>Man</h4>
-                    <a href="#" class="card-link">Exempel plagg</a>
-                    <p>Exempel pris</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 float-left">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="http://via.placeholder.com/350x350"
-                    alt="Card image cap"
-                  />
-                  <div class="card-body" id="cardBody">
-                    <h4>Kvinna</h4>
-                    <a href="#" class="card-link">Exempel plagg</a>
-                    <p>Exempel pris</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 float-left">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="http://via.placeholder.com/350x350"
-                    alt="Card image cap"
-                  />
-                  <div class="card-body" id="cardBody">
-                    <h4>Barn</h4>
-                    <a href="#" class="card-link">Exempel plagg</a>
-                    <p>Exempel pris</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="carousel-item">
-              <div class="col-md-4 float-left">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="http://via.placeholder.com/350x350"
-                    alt="Card image cap"
-                  />
-                  <div class="card-body" id="cardBody">
-                    <h4>Man</h4>
-                    <a href="#" class="card-link">Exempel plagg</a>
-                    <p>Exempel pris</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 float-left">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="http://via.placeholder.com/350x350"
-                    alt="Card image cap"
-                  />
-                  <div class="card-body" id="cardBody">
-                    <h4>Kvinna</h4>
-                    <a href="#" class="card-link">Exempel plagg</a>
-                    <p>Exempel pris</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-4 float-left">
-                <div class="card">
-                  <img
-                    class="card-img-top"
-                    src="http://via.placeholder.com/350x350"
-                    alt="Card image cap"
-                  />
-                  <div class="card-body" id="cardBody">
-                    <h4>Barn</h4>
-                    <a href="#" class="card-link">Exempel plagg</a>
-                    <p>Exempel pris</p>
-                  </div>
-                </div>
-              </div>
+  <div class="container text-center my-3">
+    <div id="recipeCarousel" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item row no-gutters active">
+          <div class="col-md-4 float-left">
+            <img class="img" src="../assets/images/men/men_2.jpg" alt="Men_1" height="200" />
+            <div class="card-body" id="cardBody">
+              <h4>Man</h4>
+              <a href="#" class="card-link">Vinterjacka</a>
+              <p>799:-</p>
             </div>
           </div>
-          <a class="carousel-control-prev" href="#ProjeCarousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-          <a class="carousel-control-next" href="#ProjeCarousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
+          <div class="col-md-4 float-left">
+            <img src="../assets/images/woman/woman_1.jpg" class="img" alt="Woman_1" height="200" />
+            <div class="card-body" id="cardBody">
+              <h4>Kvinna</h4>
+              <a href="#" class="card-link">Vinterjacka</a>
+              <p>699:-</p>
+            </div>
+          </div>
+          <div class="col-md-4 float-left">
+            <img
+              src="../assets/images/children/children_2.jpg"
+              class="img"
+              alt="Children_1"
+              height="200"
+            />
+            <div class="card-body" id="cardBody">
+              <h4>Barn</h4>
+              <a href="#" class="card-link">Vinterjacka</a>
+              <p>499:-</p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item row no-gutters">
+          <div class="col-md-4 float-left">
+            <img src="../assets/images/men/men_6.jpg" class="img" alt="Men_2" height="200" />
+            <div class="card-body" id="cardBody">
+              <h4>Man</h4>
+              <a href="#" class="card-link">Vinterskor</a>
+              <p>899:-</p>
+            </div>
+          </div>
+          <div class="col-md-4 float-left">
+            <img src="../assets/images/woman/woman_4.jpg" class="img" alt="Woman_2" height="200" />
+            <div class="card-body" id="cardBody">
+              <h4>Kvinna</h4>
+              <a href="#" class="card-link">Vinterskor</a>
+              <p>999:-</p>
+            </div>
+          </div>
+          <div class="col-md-4 float-left">
+            <img
+              src="../assets/images/children/children_6.jpg"
+              class="img"
+              alt="Children_2"
+              height="200"
+            />
+            <div class="card-body" id="cardBody">
+              <h4>Barn</h4>
+              <a href="#" class="card-link">Termobyxor</a>
+              <p>599:-</p>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item row no-gutters">
+          <div class="col-md-4 float-left">
+            <img src="../assets/images/men/men_8.jpg" class="img" alt="Men_3" height="200" />
+            <div class="card-body" id="cardBody">
+              <h4>Man</h4>
+              <a href="#" class="card-link">Mössa</a>
+              <p>299:-</p>
+            </div>
+          </div>
+          <div class="col-md-4 float-left">
+            <img src="../assets/images/woman/woman_9.jpg" class="img" alt="Woman_3" height="200" />
+            <div class="card-body" id="cardBody">
+              <h4>Kvinna</h4>
+              <a href="#" class="card-link">Mössa</a>
+              <p>299:-</p>
+            </div>
+          </div>
+          <div class="col-md-4 float-left">
+            <img
+              src="../assets/images/children/children_7.jpg"
+              class="img"
+              alt="Children_3"
+              height="200"
+            />
+            <div class="card-body" id="cardBody">
+              <h4>Barn</h4>
+              <a href="#" class="card-link">Mössa</a>
+              <p>199:-</p>
+            </div>
+          </div>
         </div>
       </div>
+      <a class="carousel-control-prev" href="#recipeCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#recipeCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
   </div>
 </template>
 
 <script>
 export default {};
+$(document).ready(function() {
+  $(".carousel").carousel({
+    interval: 5000,
+    pause: 'hover'
+  });
+});
 </script>
 
 <style>
-#ProjeCarousel {
-  margin-top: 2em;
-}
-
-#cardBody {
-  text-align: center;
-}
 </style>
