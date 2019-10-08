29c9bc1ce24b33b5b2ad49f5f483084ba1b879be
Filip Glisovic <filipglisovic@desktop-n1lisjg.learnet.se>
2019-10-08 11:29:00 +0200

    Added LoginDropdown component

---
+++ src/components/LoginDropdown.vue
@@ -0,0 +1,40 @@
+<template>
+  
+<div class="container-fluid">
+    
+
+  <form class="px-4 py-3">
+    <div class="form-group">
+      <label for="FormEmail">Email address</label>
+      <input type="email" class="form-control" id="FormEmail" placeholder="email@example.com">
+    </div>
+    <div class="form-group">
+      <label for="FormPassword">Lösenord</label>
+      <input type="password" class="form-control" id="FormPassword" placeholder="Password">
+    </div>
+    <div class="form-group">
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="Check">
+        <label class="form-check-label" for="Check">
+          Kom ihåg mig
+        </label>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Logga in</button>
+    <a href="#">Har du inget konto? Klicka här!</a>
+  <a href="#">Glömt bort lösenord?</a>
+  </form>
+
+  
+</div>
+
+
+
+
+</template>
+
+
+
+<script>
+    
+</script>
