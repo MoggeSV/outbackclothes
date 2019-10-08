516460eb280b4892989e18cf731cbcc534ac85a5
Peter Dahlin <peterdaliandalledahlin@gmail.com>
2019-10-08 14:01:07 +0200

    fixed autumnsale mediaquery

--- src/components/AutumnSale.vue
+++ src/components/AutumnSale.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container-fluid p-0">
         <div class="jumbotron  ">
-            <div class="container p-0">
-                <h1 class="display-2 text-white pt-3">Höstrea!</h1>
-                <p class="lead text-white">Spara upp til 50%.</p>
+            <div class="container p-0 pt-3">
+                <h1 class="display-2 text-white">Höstrea!</h1>
+                <p class="lead text-white">Spara upp till 50%.</p>
                 <router-link class="btn btn-primary btn-sm" to="/" role="button">Fynda nu!</router-link>
             </div>
 
@@ -33,5 +33,20 @@ export default {
     .btn:hover {
         filter: brightness(140%);
     }
+}
+@media (max-width: 600px) {
+    .jumbotron {
+        .display-2 {
+            font-size: 40px;
+            margin-left: 1rem;
+        }
+        .lead {
+            margin-left: 1rem;
+        }
+        .btn {
+            margin-left: 1rem;
+        }
+    }
+
 }
 </style>
